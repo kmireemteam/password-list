827d82a0efb446de721564da3b17bebd3540ef99
kmireemteam <80388152+kmireemteam@users.noreply.github.com>
2021-03-10 13:13:20 +0330

    Add files via upload

---
+++ kimeerteam.pass.py
@@ -0,0 +1,25 @@
+
+class color:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW='\033[93m'
+    WHITE='\033[0m'
+print("PASSLIST MAKER")
+print(color.GREEN+"Created by "+color.YELLOW+"|\/|@|-||]!"+color.GREEN+" & "+color.YELLOW+"@r!@|\|"+color.GREEN+" in kimeerteam Group")
+print()
+print()
+print()
+n = input(color.YELLOW+"please Enter the country code---->(+)  ")
+d = input (color.WHITE+"please Enter the desired prefix---->(0) ") 
+file=open("kimeerteam.txt","w")
+h=1
+k=0
+u=0
+for i in range(1111111,10000001):
+    file.write("0"+str(d)+str(i)+"\n")
+        
+    h=h+1
+print("kimeerteam.txt is your passlist")
+ 
+
+
